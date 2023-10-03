3b3619ed1c0e271f3022daffe099825394324d81
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2023-10-03 19:50:11 +0300

    Correction of a grammatical mistake that was making the checker not to work

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 string_number = str(number)
 last_digit = int(string_number[-1:])
-print("The last digit of {} is {}".format(number, last_digit), end=" ")
+print("Last digit of {} is {}".format(number, last_digit), end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
