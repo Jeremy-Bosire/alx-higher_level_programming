0562c89ad364f9c4d09ea5794ab1e39a5ee2d9d6
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2024-01-04 23:20:31 +0300

    My first ALX commit after a very long time

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-string_number = str(number)
-last_digit = int(string_number[-1:])
-print("Last digit of {} is".format(number), end=" ")
-if number >= 0:
-    if last_digit > 5:
-        print("{} and is greater than 5".format(last_digit))
-    elif last_digit == 0:
-        print("{} and is 0".format(last_digit))
-    elif last_digit < 6:
-        print("{} and is less than 6 and not 0".format(last_digit))
-elif number < 0:
-    last_digit = "-" + str(last_digit)
-    last_digit = int(last_digit)
-    if last_digit == 0:
-        print("{} and is 0".format(last_digit))
-    elif last_digit < 6:
-        print("{} and is less than 6 and not 0".format(last_digit))
+digit = number % 10
+if digit > 5:
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+elif digit == 0:
+    print(f"Last digit of {number} is {digit} and is 0")
+else:
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
