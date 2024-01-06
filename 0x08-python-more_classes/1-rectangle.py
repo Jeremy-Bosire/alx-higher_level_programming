81e9d449ae51897cd073088408fe7fd7373a9b12
Jeremy Bosire <114059541+Jeremy-Bosire@users.noreply.github.com>
2024-01-07 01:51:46 +0300

    Addition of docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,50 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class
+"""
+
+
 class Rectangle:
+    """
+    This class defines the attributes of a rectangle
+
+    Attributes:
+        width: width of the rectangle
+        height: height of the rectangle
+    """
     def __init__(self, width=0, height=0):
+        """
+        Creates new instances of class rectangle
+
+        Args:
+            width (int, optional): width of rectangle. Defaults to 0.
+            height (int, optional): height of rectangle. Defaults to 0.
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """
+        Width getter.
+
+        Returns:
+            int: the width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Property setter for width of rectangle
+
+        Args:
+            value (int): width of the rectangle.
+
+        Exceptions:
+            TypeError: if width is not an integer
+            ValueError: if width is less than zero
+        """
         try:
             value % 1 == 0
         except TypeError:
@@ -21,10 +56,26 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        Height getter.
+
+        Returns:
+            int: the height of the rectangle.
+        """
         return self.__height
 
     @height.setter
-    def height(self):
+    def height(self, value):
+        """
+        Property setter for height of rectangle
+
+        Args:
+            value (int): height of the rectangle.
+
+        Exceptions:
+            TypeError: If height is not an integer
+            ValueError: If height is less than Zero
+        """
         try:
             value % 1 == 0
         except TypeError:
@@ -33,4 +84,3 @@ class Rectangle:
             value > 0
         except ValueError:
             print("height must be >= 0")
-
